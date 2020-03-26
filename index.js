18603481a9db5ad7f197aa96d9c07c31d4972933
Travis Kopp <teakopp@github.com>
2020-03-26 16:23:14 -0700

    log commenter's login specifically

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const myToken = core.getInput('GITHUB_TOKEN');
 const octokit = new github.GitHub(myToken);
 const context = github.context;
 
-console.log(context.payload.comment.user);
+console.log(context.payload.comment.user.login);
 console.log(context.payload.comment.body);
 
