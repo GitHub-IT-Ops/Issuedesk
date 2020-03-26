996e7b0a81552a61995b986e2762ef269f994ed1
Travis Kopp <teakopp@github.com>
2020-03-26 16:40:03 -0700

    get issue url

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const myToken = core.getInput('GITHUB_TOKEN');
 const octokit = new github.GitHub(myToken);
 const context = github.context;
 
-console.log(context);
-console.log("\n----\n");
+
+
 console.log(context.payload.comment.user.login);
 console.log(context.payload.comment.body);
+console.log(`To see issue, click [here](${context.payload.issue.url})`);
+
 
