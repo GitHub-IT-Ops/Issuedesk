ba8333dd4d2ec638b0f93abf37b8aba42f19dee9
Travis Kopp <teakopp@github.com>
2020-03-26 17:37:04 -0700

    logging context to find issue labels

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const core = require('@actions/core');
 const myToken = core.getInput('GITHUB_TOKEN');
 const octokit = new github.GitHub(myToken);
 const context = github.context;
-
-
-
+console.log(context.payload);
+console.log("\n");
 console.log(context.payload.comment.user.login);
 console.log(context.payload.comment.body);
 console.log(`To see issue, click [here](${context.payload.issue.url})`);
