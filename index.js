ccbc186a25346f3c1c00daa01ccf9e8c0036004c
Travis Kopp <teakopp@github.com>
2020-04-01 11:26:48 -0700

    test how github object works inside class

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const core = require('@actions/core');
 const myToken = core.getInput('GITHUB_TOKEN');
 const octokit = new github.GitHub(myToken);
 const context = github.context;
+const GitZen = require('./lib/gitzen.js').GitZen
+const gitzen = new GitZen()
+
+let ticketTitle = context.payload
 console.log(context.payload);
 console.log("\n");
 console.log(context.payload.comment.user.login);
 console.log(context.payload.comment.body);
 console.log(`To see issue, click [here](${context.payload.issue.url})`);
+
+console.log(gitzen.getIssueThread("teakopp", "it-bot", 1));
+
 
 
