cddbd72aa7e8e245da3015c085a7b2af5aa6c118
Travis Kopp <teakopp@github.com>
2020-06-16 14:39:37 -0700

    fix eslint chaos

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const core = require("@actions/core");
-const myToken = core.getInput("GITHUB_TOKEN");
-const zendeskUsername = core.getInput("ZENDESK_USERNAME");
-const zendeskToken = core.getInput("ZENDESK_TOKEN");
-const zendeskURI = core.getInput("ZENDESK_URI");
+const core = require('@actions/core')
+const myToken = core.getInput('GITHUB_TOKEN')
+const zendeskUsername = core.getInput('ZENDESK_USERNAME')
+const zendeskToken = core.getInput('ZENDESK_TOKEN')
+const zendeskURI = core.getInput('ZENDESK_URI')
 
-const Gitzen = require("./lib/gitzen.js").Gitzen;
-const gitzen = new Gitzen(myToken, zendeskUsername, zendeskToken, zendeskURI);
+const Gitzen = require('./lib/gitzen.js').Gitzen
+const gitzen = new Gitzen(myToken, zendeskUsername, zendeskToken, zendeskURI)
 
-asyncMain();
+asyncMain()
 
 async function asyncMain() {
-  const ticketAlreadyExists = gitzen.doesTicketAlreadyExist();
+    const ticketAlreadyExists = gitzen.doesTicketAlreadyExist()
 
-  if (ticketAlreadyExists) {
-    console.log("Ticket already exists! Exiting...");
-  } else {
-    console.log("This would create a ticket");
+    if (ticketAlreadyExists) {
+        console.log('Ticket already exists! Exiting...')
+    } else {
+        console.log('This would create a ticket')
 
-    // await gitzen.generateTicket();
-    // gitzen.createTicket();
-  }
+        // await gitzen.generateTicket();
+        // gitzen.createTicket();
+    }
 }
