d42b25b79e5936ac5bf6dc51453eb934b77da419
Travis Kopp <teakopp@github.com>
2020-06-29 16:14:22 -0700

    added test for issuedesk

--- __tests__/issuedesk.test.js
+++ __tests__/issuedesk.test.js
@@ -1,15 +1,15 @@
 const IssueDesk = require('../lib/issuedesk.js').IssueDesk
 const IssueMonitor = require('../lib/issuemonitor.js').IssueMonitor
 const TicketMaker = require('../lib/ticketmaker.js').TicketMaker
-const mockContext = require('../__mocks__/eventIssueComment.json')
-const mockListOfComments = require('../__mocks__/getIssueComments.json')
 const myToken = 'gitHubActionsToken'
 const zendeskUsername = 'test@test.com'
 const zendeskToken = "EEEEEeeeeee9eeE9EEeeE99eeEeeee9e99eeEEEe"
 const zendeskURI = 'https://test.zendesk.com/api/v2/'
 jest.mock('../lib/issuemonitor.js')
 jest.mock('../lib/ticketmaker.js')
-
+jest.mock('@actions/github')
+jest.mock('node-zendesk')
+jest.mock('../__mocks__/octokit.js')
 
 afterEach(() => {
     jest.clearAllMocks()
@@ -23,5 +23,6 @@ test("IssueMonitor & TicketMaker are both instantiated in monitorIssueAndMakeTic
     expect(TicketMaker).toHaveBeenCalled();
 
 })
+
 
 
