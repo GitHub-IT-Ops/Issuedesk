97b4b006bc8846a708fdd323dc750b1efe8f4359
Travis Kopp <teakopp@github.com>
2020-07-01 10:14:28 -0700

    added comments to index.js & issuedesk.ts

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const zendeskUsername = core.getInput('ZENDESK_USERNAME')
 const zendeskToken = core.getInput('ZENDESK_TOKEN')
 const zendeskURI = core.getInput('ZENDESK_URI')
 const activationLabel = core.getInput('ACTIVATION_LABEL')
-
 const IssueDesk = require('./lib/issuedesk.js').IssueDesk
+
+//Creates instance of issuedesk, which creates instances of other classes to handle operations
 const issuedesk = new IssueDesk(
     myToken,
     zendeskUsername,
@@ -14,4 +15,5 @@ const issuedesk = new IssueDesk(
     activationLabel
 )
 
+//Handles "Issue Labeled" events. Passed variable in via Actions variables, because I wanted to make it customizeable for other users. Feel free to pass in more efficent way, if possible.
 issuedesk.monitorIssueAndMakeTicket(activationLabel)
