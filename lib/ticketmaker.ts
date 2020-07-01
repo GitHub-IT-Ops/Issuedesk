0be40d748c72e262f049babda6e29680c18e566a
Travis Kopp <teakopp@github.com>
2020-07-01 10:53:38 -0700

    added commentsa to zendesk monitor.ts & ticketmaker.ts

--- lib/ticketmaker.ts
+++ lib/ticketmaker.ts
@@ -1,5 +1,7 @@
 import { ticketType, listOfCommentsType } from '../types/types.js'
 
+// class exists to generate and then create ticket in zendesk 
+// ticket is created and stored in this.ticket so that operations in this class can modify it
 class TicketMaker {
     client: any
     ticket: ticketType
@@ -14,10 +16,13 @@ class TicketMaker {
         return this.ticket
     }
 
+    //currently used to link Zendesk Ticket to github issue by setting external_id to issue's html_url
+    // This will be useful when two communication between zendesk and github is enabled
     private setExternalId(externalId: string) {
         return (this.ticket['ticket']['external_id'] = externalId)
     }
 
+    // creates ticket body from info passed to it by github api
     public async generateTicketBody(
         listOfComments: [listOfCommentsType],
         issueUrl: string
@@ -36,11 +41,14 @@ class TicketMaker {
         return ticketBody
     }
 
+    // Sets ticket subject to string, usually set to same name as issue
     public generateTicketSubject(issueTitle: string) {
         this.ticket['ticket']['subject'] = issueTitle
         return this.ticket
     }
 
+    // Generates ticket in this.ticket using arguments passed in.
+    // Ticket is ready to be sent to zendesk after this is called
     public async generateTicket(
         issueUrl: string,
         issueTitle: string,
@@ -51,6 +59,7 @@ class TicketMaker {
         await this.generateTicketBody(listOfComments, issueUrl)
     }
 
+    //Creates ticket in zendesk. Should be called after ticket is generated using generateTicket()
     private createTicket() {
         this.client.tickets.create(
             this.ticket,
@@ -68,7 +77,8 @@ class TicketMaker {
     }
 
     // I hate that this function is neccesary. This is one of the 3 functions made to offset the bug in node-zendesk that prevents promises from working.
-    // This function exists soley to complete the creation process by nesting inside doesTicketAlreadyExist(). Remove as soon as possible in favor of async / await
+    // This function exists soley to complete the creation process by nesting inside createTicketIfItDoesNotExist(). Remove as soon as possible in favor of async / await
+    // Handles entirety of ticket creation process, by generating ticket using passed in args and then calling createTicket() to actually create it in zendesk
     private async ticketCreation(
         issueUrl: string,
         issueTitle: string,
@@ -78,6 +88,8 @@ class TicketMaker {
         this.createTicket()
     }
 
+    // Makes sure ticket doesn't exist, but will create another ticket if ticket containing same external_id, but has status solved or closed
+    // This was done to protect against accidentally closed or solved tickets. Once two way communication is implemented between zendesk and github, this will be immensly important
     private doesTicketAlreadyExist(
         ticket: { [x: string]: any },
         issueUrl: string
@@ -98,6 +110,8 @@ class TicketMaker {
     // Split into a seperate function due to node-zendesks 2.0.0 & 1.5.0 version issues at the time.
     // Use of callbacks like this function and createTicketIfItDoesNotExist() should only be used until issue is resolved
     // in node-zendesk library.
+    // Handles entirety of ticket creation process. uses this.client.tickets.list to load all tickets and then runs them through doesTicketAlreadyExist() to make sure duplicate tickets aren't created, if
+    // ticket already exist on zendesk. Use this single function to handle all of creation process until bug is solved.
 
     public createTicketIfItDoesNotExist(
         issueUrl: string,
--- lib/zendeskmonitor.ts
+++ lib/zendeskmonitor.ts
@@ -1,3 +1,5 @@
+
+//This class exists to handle all zendesk related events and data. Will eventually split some of ticketmaker.ts into this class once node-zendesk bug is fixed
 class ZendeskMonitor {
     client: any
     constructor(client: any) {
