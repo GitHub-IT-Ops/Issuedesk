0c55bf31d71de660db7710244300547d0e97a64c
Travis Kopp <teakopp@github.com>
2020-07-28 18:45:58 -0700

    imp[lemented Promise to fix callback issue with node-zendesk in getAllZendeskTickets() in zendeskmonitor.ts

--- lib/zendeskmonitor.ts
+++ lib/zendeskmonitor.ts
@@ -66,7 +66,6 @@ class ZendeskMonitor {
     // ticket already exist on zendesk. Use this single function to handle all of creation process until bug is solved.
     public async createTicketIfItDoesNotExist(ticket: ticketType) {
         const allZendeskTickets = await this.getAllZendeskTickets()
-        console.log(allZendeskTickets)
 
         for (let i = 0; i < (allZendeskTickets.length); i++) {
             console.log('loop started')
